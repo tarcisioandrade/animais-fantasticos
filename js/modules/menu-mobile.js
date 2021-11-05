34e2b77f6d190c17662fc4e928c1710c419a482f
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2021-11-05 16:50:20 -0300

    Primeiro Commit

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,16 @@
+import outsideClick from "./outsideclick.js";
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector("[data-menu='button']");
+  const menuList = document.querySelector("[data-menu='list']");
+  const eventos = ["click", "touchstart"];
+  function openMenu() {
+    menuList.classList.add("active");
+    menuButton.classList.add("active");
+    outsideClick(menuList, ["click", "touchstart"], () => {
+      menuList.classList.remove("active");
+      menuButton.classList.remove("active");
+    });
+  }
+  eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+}
